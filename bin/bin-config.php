77b0504e8cad8dd9bf01cebc35a13c81279c027a
Timothy M. Crider <timcrider@gmail.com>
2012-08-21 07:14:31 -0700

    Adding in shell script envionrment loader

---
+++ bin/bin-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+* Shell script configuration and environment loading
+*/
+
+// Custom pre-environment functionalty
+define('ISBIN', true);
+// End pre-environment
+
+require_once dirname(dirname(__FILE__)).'/environment.php';
+
+// Custom post-environment functionalty
+
+// End post-environment
