557943e419d5711e79f92896f119187cbfd93d99
Timothy M. Crider <timcrider@gmail.com>
2012-08-22 18:22:48 -0700

    Updating default configuration template with encoding and case folding options

--- config/configuration.default.php
+++ config/configuration.default.php
@@ -9,16 +9,18 @@
 $config->database = array(
 	'adapter'    => 'Pdo_MySQL',
 	'connection' => array(
-		'host'     => '',
-		'username' => '',
-		'password' => '',
-		'dbname'   => 'fw42',
-		'profiler' => false,
+		'host'           => '',
+		'username'       => '',
+		'password'       => '',
+		'dbname'         => 'fw42',
+		'charset'        => 'utf8',
+		'profiler'       => false,
 		'driver_options' => array(
 			PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"
 		)
 	),
 	'options'    => array(
+		'case_folding'      => Zend_Db::CASE_NATURAL,
 		'quote_identifiers' => true
 	)
 );
